2405f8854f16a44013ffe39aa334f1048dbd3812
ahmedali839 <ahmedyarr1212@gmail.com>
2026-01-24 01:03:32 +0500

    more structure and union in c++

--- structure/structure.cpp
+++ structure/structure.cpp
@@ -29,3 +29,47 @@ int main()
     clg.id = 02;
     cout << "id: " << clg.id << " and It's memory address value:  " << &clg.id << endl;
 }
+
+
+
+// // // Another/Alternative Example:
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// struct Student
+// {
+//     string name;
+//     int id;
+// };
+
+// union College
+// {
+//     // Change string to char* (C-style string)
+//     // NOTE: This just points to a string, it doesn't manage the memory itself like std::string
+//     const char* name; 
+//     int id;
+// };
+
+// int main()
+// {
+//     Student stud = {"Ahmed", 1};
+    
+//     // Now this works!
+//     College clg;
+//     clg.name = "Ali"; // Union now holds a name
+    
+//     cout << "Structure Values: " << endl;
+//     cout << "Name: " << stud.name << " & Address: " << &stud.name << endl;
+//     cout << "ID: " << stud.id << " & Address: " << &stud.id << endl;
+    
+//     cout << "\nUnion Values: " << endl;
+//     cout << "Name: " << clg.name << " & Address: " << &clg.name << endl;
+    
+//     // WARNING: In a union, writing to 'id' will overwrite 'name' because they share memory!
+//     clg.id = 2; 
+//     cout << "ID: " << clg.id << " & Address: " << &clg.id << endl;
+    
+//     // If you try to print clg.name now, it will likely be garbage because clg.id overwrote it.
+// }
